d4c8ed65bc0a15114b15a5750b93513b4d6b8ccd
David Golke <golkedj@gmail.com>
2017-01-18 08:06:59 -0600

    Section: 10 Lecture 76
    Course completed

--- index.php
+++ index.php
@@ -340,7 +340,7 @@
                     <form method="post" action="mailer.php" class="contact-form">
                         <div class="row">
                             
-                            <!--<?php
+                            <?php
                             if($_GET['success']) == 1) {
                                 echo "<div class=\"form-messages success\">Thank you! Your message has been sent.</div>"
                             }
@@ -348,7 +348,7 @@
                             if($_GET['success']) == -1) {
                                 echo "<div class=\"form-messages success\">Oops! Something went wrong. Please try again!</div>"
                             }                            
-                            ?>-->
+                            ?>
                         </div>
                         <div class="row">
                             <div class="col span-1-of-3">
